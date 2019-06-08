f9f8dd6c7d52b9cbb9545d5f3b36063b828832b1
singer233 <sht852680833@gamil.com>
2019-06-08 17:47:47 +1000

    add error notice for internet error

--- program/tool.py
+++ program/tool.py
@@ -4,6 +4,7 @@ def gethtml(url):
     headers={'User-Agent':'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
     webpage = requests.get(url,headers=headers)
     if webpage.status_code != 200 :
+            print('website error return code: '+ str(webpage.status_code))
             exit(-1)
     return webpage.text
 def filewrite(string,fl):
